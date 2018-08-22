48a35b0327f190ef668ebfbcc22c47002398b9b2
Willsi <willsi.cooper@googlemail.com>
2018-08-22 13:22:08 +0000

    terraform

---
+++ resources.tf
@@ -0,0 +1,33 @@
+resource "google_compute_instance" "default" {
+	name = "test"
+	machine_type = "n1-standard-1"
+	zone = "europe-west2-c"
+	boot_disk {
+		initialize_params {
+			image = "centos-7"
+			}
+		}
+	network_interface {
+		network = "default"
+		access_config {
+			// Ephemeral0
+			}
+		}
+
+	metadata {
+		sshKeys = "terraform:${file("~/.ssh/id_rsa.pub")}"
+		}
+	provisioner "remote-exec" {
+		connection = {
+			type = "ssh"
+			user = "terraform"
+			private_key = "${file("~/.ssh/id_rsa")}"
+			}
+	scripts = [ 
+		"scripts/test1",
+		"scripts/test2"
+		]
+	
+	
+	}
+}
